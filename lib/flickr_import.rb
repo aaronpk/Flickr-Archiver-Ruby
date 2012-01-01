dd15bee60311ddb298c5bf51231114059cd22ab8
Aaron Parecki <aaron@parecki.com>
2011-12-31 20:13:03 -0700

    Prevent re-downloading the JPGs unless the files actually change.

--- lib/flickr_import.rb
+++ lib/flickr_import.rb
@@ -92,7 +92,7 @@ class FlickrImport
     end
 
     totalPages = photos.pages
-    puts "====== Found #{totalPages} pages"
+    puts "====== Found #{photos.total} photos in #{totalPages} pages"
 
     (1..totalPages).each do |page|
       puts
@@ -120,74 +120,89 @@ class FlickrImport
         puts flickrPhoto.to_hash.to_json
 
         if photo.nil?
+          previousSecret = nil
           # If an existing photo record was not found, prepare a new one
           photo = Photo.create_from_flickr flickrPhoto, @user
         else
+          previousSecret = photo.secret
           # Else update the record from the flickr info
           photo.update_from_flickr flickrPhoto
         end
 
-        photo.url = FlickRaw.url_photopage(flickrPhoto)
-        Photo.sizes.each do |s|
-          if p.respond_to?('url_'+s)
-            flickrURL = p.send('url_'+s)
-
-            # Store the original flickr URLs
-            photo.send('url_'+s+'=', flickrURL)
-            photo.send('width_'+s+'=', p.send('width_'+s))
-            photo.send('height_'+s+'=', p.send('height_'+s))
-
-            # Make the parent folder
-            FileUtils.mkdir_p(photo.abs_path(s))
-            local_abs_filename = photo.abs_filename(s)
-
-            new_local_path = photo.path(s)+photo.filename(s)
-
-            # If the photo was renamed, delete the old file first
-            old_local_path = photo.send("local_path_#{s}")
-            if old_local_path && new_local_path != old_local_path
-              old_abs_filename = SiteConfig.photo_root + old_local_path
-              puts "! Deleting #{old_local_path}"
-              `rm #{old_abs_filename}`
-            end
-
-            photo.send("local_path_#{s}=", new_local_path)
-
-            # Download file from Flickr
-            puts "Downloading #{flickrURL} to #{local_abs_filename}"
-            `curl -o #{local_abs_filename} #{flickrURL}`
-            puts "...done"
-          end
+        # Determine whether to download files. During import, this is always. During updates,
+        # only download if the secret has changed. The secret changing indicates that there is
+        # a new jpg file (i.e. the photo was rotated or replaced)
+        if mode == 'import'
+          should_download = true
+        else
+          should_download = previousSecret != photo.secret
         end
 
-        # If it's a video, download the file
-        if flickrPhoto.media == "video"
-          FileUtils.mkdir_p(photo.abs_path('v'))
-          local_abs_filename = photo.abs_filename('v')
+        if should_download
+          photo.url = FlickRaw.url_photopage(flickrPhoto)
+          Photo.sizes.each do |s|
+            if p.respond_to?('url_'+s)
+              flickrURL = p.send('url_'+s)
 
-          # The secret seems to be different for the original version of the video. Not sure how to find it
-          # videoURL = "http://www.flickr.com/photos/#{@user.nsid}/#{photo.flickr_id}/play/orig/#{photo.secret}/"
+              # Store the original flickr URLs
+              photo.send('url_'+s+'=', flickrURL)
+              photo.send('width_'+s+'=', p.send('width_'+s))
+              photo.send('height_'+s+'=', p.send('height_'+s))
 
-          # Instead, iterate through all the available sizes and look for the largest available
-          flickrSizes = @flickr.photos.getSizes :photo_id => p.id
-          largestSize = 0
-          videoURL = ''
-          flickrSizes.size.each do |sz|
-            if sz.media == "video"
-              if sz.width.to_i > largestSize
-                largestSize = sz.width.to_i
-                videoURL = sz.source
+              # Make the parent folder
+              FileUtils.mkdir_p(photo.abs_path(s))
+              local_abs_filename = photo.abs_filename(s)
+
+              new_local_path = photo.path(s)+photo.filename(s)
+
+              # If the photo was renamed, delete the old file first
+              old_local_path = photo.send("local_path_#{s}")
+              if old_local_path && new_local_path != old_local_path
+                old_abs_filename = SiteConfig.photo_root + old_local_path
+                puts "! Deleting #{old_local_path}"
+                `rm #{old_abs_filename}`
+              end
+
+              photo.send("local_path_#{s}=", new_local_path)
+
+              # Download file from Flickr
+              puts "Downloading #{flickrURL} to #{local_abs_filename}"
+              `curl -o #{local_abs_filename} #{flickrURL}`
+              puts "...done"
+            end
+          end # end each size
+
+          # If it's a video, download the file
+          if flickrPhoto.media == "video"
+            FileUtils.mkdir_p(photo.abs_path('v'))
+            local_abs_filename = photo.abs_filename('v')
+
+            # The secret seems to be different for the original version of the video. Not sure how to find it
+            # videoURL = "http://www.flickr.com/photos/#{@user.nsid}/#{photo.flickr_id}/play/orig/#{photo.secret}/"
+
+            # Instead, iterate through all the available sizes and look for the largest available
+            flickrSizes = @flickr.photos.getSizes :photo_id => p.id
+            largestSize = 0
+            videoURL = ''
+            flickrSizes.size.each do |sz|
+              if sz.media == "video"
+                if sz.width.to_i > largestSize
+                  largestSize = sz.width.to_i
+                  videoURL = sz.source
+                end
               end
             end
-          end
 
-          puts "Downloading #{videoURL} to #{local_abs_filename}"
-          `curl -L -o #{local_abs_filename} #{videoURL}`
-          puts "...done"
+            puts "Downloading #{videoURL} to #{local_abs_filename}"
+            `curl -L -o #{local_abs_filename} #{videoURL}`
+            puts "...done"
 
-          photo.local_path_v = photo.filename('v')
-          photo.media = "video"
-        end
+            photo.local_path_v = photo.filename('v')
+            photo.media = "video"
+          end # end if video
+        else
+          puts "\twon't download file"
+        end # end if should download
 
         owner = Person.first :nsid => flickrPhoto.owner.nsid, :user => @user
         if owner.nil?
@@ -275,16 +290,16 @@ class FlickrImport
         # In 'import' mode, this relies on the photos being returned in descending order.
         if mode == 'import'
           @user.import_timestamp = photo.date_uploaded.to_time.to_i
+          @user.last_photo_imported = p.id
+          @user.save
         end
-        @user.last_photo_imported = p.id
-        @user.save
 
       end   # for each photos
 
     end
 
     if mode == 'update'
-      @user.import_timestamp = updateStartedAt
+      @user.import_timestamp = updateStartedAt.to_time.to_i
       @user.save
     end
 
