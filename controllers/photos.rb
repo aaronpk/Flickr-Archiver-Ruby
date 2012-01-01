7bed1a27279338100184cdcd497513841bb6623d
Aaron Parecki <aaron@parecki.com>
2012-01-01 11:06:30 -0700

    Remove unused route

--- controllers/photos.rb
+++ controllers/photos.rb
@@ -35,34 +35,6 @@ get '/:username/?' do
   end
 end
 
-get '/:username/recent' do
-  begin
-    load_user params[:username]
-    begin
-      since = Time.now.to_i - 86400
-      photos = @flickr.photos.recentlyUpdated :min_date => since, :extras => 'description,license,date_upload,date_taken,owner_name,original_format,last_update,geo,tags,machine_tags,o_dims,views,media,path_alias,url_sq,url_t,url_s,url_m,url_z,url_l,url_o'
-      @photos = []
-      photos.each do |photo|
-        p = {}
-        p['class'] = "public"
-        p['class'] = "private" if photo.ispublic == 0 && photo.isfriend == 0 && photo.isfamily == 0
-        p['class'] = "friend" if photo.isfriend == 1
-        p['class'] = "family" if photo.isfamily == 1
-        p['class'] = "family friend" if photo.isfamily == 1 && photo.isfriend == 1
-        p['photo'] = photo
-        @photos.push p
-      end
-      erb :'photos/recent'
-    rescue FlickRaw::FailedResponse => e
-      puts "Error : #{e.msg}"
-      @error = e
-      erb :flickr_error
-    end
-  rescue FlickrArchivr::Error => e
-    erb :"#{e.erb_template}"
-  end
-end
-
 get '/:username/photo/:id/?*' do
   begin
     load_user params[:username]
